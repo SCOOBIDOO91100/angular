09cf8a6dcbce3a7ed2c44fe24250a84ef5263274
Nabil Hafid Alaoui <nabil.hafidalaoui@gmail.com>
2019-05-31 16:35:33 +0200

    secon commit

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -10,9 +10,11 @@ import { MessageService } from './message.service';
 })
 export class HeroService {
 
-  constructor() { }
+  constructor(private messageService: MessageService) { }
 
   getHeroes(): Observable<Hero[]> {
+    // TODO: send the message _after_ fetching the heroes
+    this.messageService.add('HeroService: fetched heroes');
     return of(HEROES);
   }
 }
